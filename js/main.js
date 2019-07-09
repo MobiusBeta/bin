aa8bcf72337244501f512084d13cc60603e07654
Mobius_Beta <405801769@qq.com>
2019-07-09 14:46:35 +0800

    19w28d

--- js/main.js
+++ js/main.js
@@ -90,6 +90,7 @@ function getPost(){
 	xhr.onreadystatechange = function(){ 
 		if (xhr.readyState==4 && xhr.status==200){ 
 			console.log(xhr.responseText);
+			postList.innerHtml=xhr.responseText;
 		}
 	}
 }
