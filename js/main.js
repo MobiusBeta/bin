431781d651340e5c99f4e1f658f9e77fcb2438b8
Mobius_Beta <405801769@qq.com>
2019-07-14 11:58:22 +0800

    19w28i3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* JavaScript written by MaoRX.cn */
-var version="19w28i2";
+var version="19w28i3";
 console.info("Version "+version);
 var backend="https://maorx.cn/bin_backend/main.php";
 var postBtnEnabled=true;
